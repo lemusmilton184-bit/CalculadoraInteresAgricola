f76d18a735ab43d0c4c9c31d860afb0fe3f9bb2e
lemusmilton184-bit <lemusmilton184@gmail.com>
2026-04-25 20:59:37 +0100

    Calculadora de interes agricula

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("=== calculadora de Interes Agricola ===");
+
+        System.out.println("Ingrese el monto del prestamo: ");
+        double monto = scanner.nextDouble();
+
+        System.out.println("Ingrese la tasa anual: ");
+        double tasaAnual = scanner.nextDouble();
+
+        System.out.println("Ingrese el plazo en meses: ");
+        int plazoMeses = scanner.nextInt();
+
+        calculadoraIteres calculoIteres = new calculadoraIteres(monto, tasaAnual, plazoMeses);
+        double total = calculoIteres.calcularInteres();
+
+        System.out.println("---------------------------------");
+        System.out.println("El interes simples a pagar es: $" + total);
+
+        scanner.close();
     }
 }
--- src/calculadoraIteres.java
+++ src/calculadoraIteres.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
-
 public class calculadoraIteres {
+
+    double monto = 0;
+    double tasaAnual = 0;
+    int plazoMeses = 0;
+
+    public calculadoraIteres(double monto, double tasaAnual, int plazoMeses) {
+        setMonto(monto);
+        setPlazoMeses(plazoMeses);
+        setTasaAnual(tasaAnual);
+    }
+
+    public void setMonto(double monto){
+        this.monto = monto;
+    }
+
+    public void setTasaAnual(double tasaAnual) {
+        this.tasaAnual = tasaAnual / 100;
+    }
+
+    public void setPlazoMeses(int plazoMeses) {
+        this.plazoMeses = plazoMeses;
+    }
+
+    public double calcularInteres(){
+
+        //Aplica descuento del 1% si es el monto es mayor a 50000
+        if (monto > 50000){
+            tasaAnual = tasaAnual - 0.01;
+        }
+
+
+
+        //Convertir en meses a años
+        double tiempo = plazoMeses / 12.0;
+
+        //calcular inereses simples
+
+        return monto * tasaAnual * tiempo;
+
+    }
+
 }
